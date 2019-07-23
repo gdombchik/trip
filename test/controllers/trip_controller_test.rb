5a74ff5738d2b27eaaf520a12b1b2efffd937316
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-23 00:07:09 -0400

    update TripControllerTest to verify the paragraph tag

--- test/controllers/trip_controller_test.rb
+++ test/controllers/trip_controller_test.rb
@@ -9,11 +9,13 @@ class TripControllerTest < ActionDispatch::IntegrationTest
   test "should get broadway_bridge" do
     get trip_broadway_bridge_url
     assert_response :success
+    assert_select "p", "Broadway Bridge"
   end
 
   test "should get korea town" do
     get trip_korea_town_url
     assert_response :success
+    assert_select "p", "Korea Town"
   end
 
 end
